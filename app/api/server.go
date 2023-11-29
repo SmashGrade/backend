3c4ae286b8ca683aa9e88f6d02ef7448d06efcaf
jakob.ferber <jakob.ferber@hftm.ch>
2023-11-29 18:11:43 +0100

    refactor: according to code Review

--- app/api/server.go
+++ app/api/server.go
@@ -8,26 +8,30 @@ import (
 )
 
 type SetupContext struct {
-	Echo echo.Echo
+	Echo *echo.Echo
 }
 
-func Setup() SetupContext {
-	return SetupContext{
-		Echo: *echo.New(),
+func NewServer() *SetupContext {
+	e := echo.New()
+
+	return &SetupContext{
+		Echo: e,
 	}
 }
 
 func (ctx *SetupContext) Run(port int) error {
 	portStr := fmt.Sprintf(":%v", port)
+
 	s := http.Server{
 		Addr:    portStr,
-		Handler: &ctx.Echo,
+		Handler: ctx.Echo,
 	}
 	err := s.ListenAndServe()
 	if err != nil {
 		return err
 	}
-	return nil
+
+	return fmt.Errorf("The Server has unexpectedly stopped")
 }
 
 func (ctx *SetupContext) Stop() error {
