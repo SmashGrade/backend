13138d2a6ec21a7944f018ec535325562446312e
Stafel <rafael.stauffer@hftm.ch>
2023-12-28 15:30:31 +0100

    feat: add migration

--- app/provider/sqlite_provider.go
+++ app/provider/sqlite_provider.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"log"
 
+	"github.com/SmashGrade/backend/app/entity"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,7 +17,25 @@ type SqliteProvider struct {
 
 // automigrates all entities
 func (s *SqliteProvider) Migrate() {
-	//db.AutoMigrate(&entity.Whatever)
+	// Migrate all free standing structs first
+	s.db.AutoMigrate(&entity.Curriculumtype{})
+	s.db.AutoMigrate(&entity.Field{})
+	s.db.AutoMigrate(&entity.Evaluationtype{})
+	s.db.AutoMigrate(&entity.Examtype{})
+	s.db.AutoMigrate(&entity.Role{})
+	s.db.AutoMigrate(&entity.State{})
+	s.db.AutoMigrate(&entity.Gradetype{})
+	// Migrate secondary structs second
+	s.db.AutoMigrate(&entity.Focus{})
+	s.db.AutoMigrate(&entity.Module{})
+	s.db.AutoMigrate(&entity.Curriculum{})
+	s.db.AutoMigrate(&entity.Course{})
+	s.db.AutoMigrate(&entity.User{})
+	// Migrate convoluted stuff last
+	s.db.AutoMigrate(&entity.SelectedCourse{})
+	s.db.AutoMigrate(&entity.Exam{})
+	s.db.AutoMigrate(&entity.ExamEvaluation{})
+	s.db.AutoMigrate(&entity.Conversion{})
 }
 
 // connect to the defaultDbPath sqlite
--- app/provider/sqlite_provider_test.go
+++ app/provider/sqlite_provider_test.go
@@ -1,1 +1,11 @@
 package provider
+
+import (
+	"testing"
+)
+
+func TestProvider(t *testing.T) {
+	prov := &SqliteProvider{}
+	prov.Connect()
+	t.Fatalf("Hello")
+}
