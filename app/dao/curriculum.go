cab141dc828c68d2f6a66ca30234ee2a20d2fdc3
stafel <rafael.stauffer@hftm.ch>
2024-01-24 19:44:21 +0100

    feat: check for field, module and fieldmanager at create curriculum

--- app/dao/curriculum.go
+++ app/dao/curriculum.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"fmt"
+
 	"github.com/SmashGrade/backend/app/api/v1/schemas"
 	"github.com/SmashGrade/backend/app/entity"
 )
@@ -36,12 +38,41 @@ func (db *Database) GetCurriculumFilter(curriculumFilter *schemas.CurriculumFilt
 // Returns id of newly created curriculum
 func (db *Database) CreateCurriculum(curriculum *schemas.CurriculumReq) (uint, error) {
 
+	// get our in between ids of field and focus from the strings
+	focus, err := db.GetFocusByDescription(curriculum.Focus)
+	if err != nil {
+		return 0, err // probably no focus with that desc
+	}
+
+	field, err := db.GetFieldByDescription(curriculum.Field)
+	if err != nil {
+		return 0, err // probably no field with that desc
+	}
+
+	// check if those assigned field managers exist
+	for _, uid := range curriculum.FieldmanagerRef {
+		user_found := false
+		for _, user := range field.Users {
+			if uid == user.ID {
+				user_found = true
+				break
+			}
+		}
+		if !user_found {
+			return 0, fmt.Errorf("no user with id '%v' found", uid)
+		}
+	}
+
+	// TODO: get modules
+
 	curriculumEntity := &entity.Curriculum{}
 
-	err := ParseSchemaToEntity(&curriculum, &curriculumEntity)
+	err = ParseSchemaToEntity(&curriculum, &curriculumEntity)
 	if err != nil {
 		return 0, err
 	}
+
+	curriculumEntity.Focus = *focus // add found focus to the autocasted curriculum
 
 	err = db.Db.Create(&curriculumEntity).Error
 	if err != nil {
