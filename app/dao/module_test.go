f3ae091ddc60ff4b6cb74f7cc93ca24ee7aa57a1
stafel <rafael.stauffer@hftm.ch>
2024-01-28 15:49:07 +0100

    test: comment out unfinished testcase for building

--- app/dao/module_test.go
+++ app/dao/module_test.go
@@ -1,14 +1,7 @@
 package dao_test
 
-import (
-	"testing"
-
-	"github.com/SmashGrade/backend/app/api/v1/schemas"
-	"github.com/SmashGrade/backend/app/dao"
-	"github.com/SmashGrade/backend/app/provider"
-)
-
 // creates a new module
+/*
 func TestCreateModule(t *testing.T) {
 	prov := &provider.SqliteProvider{}
 	prov.Connect()
@@ -24,5 +17,6 @@ func TestCreateModule(t *testing.T) {
 		t.Fatalf("Could not create module, err: %v\n", err.Error())
 	}
 
-	db.GetModule(, id, )
+	//db.GetModule(, id, )
 }
+*/
