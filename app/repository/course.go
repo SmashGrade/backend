80560015025067298deb16ab3b90708e7dbe30bf
jakob.ferber <jakob.ferber@hftm.ch>
2024-02-09 13:20:05 +0100

    feat: added Course Repository

---
+++ app/repository/course.go
@@ -0,0 +1,62 @@
+package repository
+
+import (
+	"github.com/SmashGrade/backend/app/db"
+	"github.com/SmashGrade/backend/app/models"
+)
+
+type CourseRepository struct {
+	Provider *db.BaseProvider
+}
+
+func NewCourseRepository(provider *db.BaseProvider) *CourseRepository {
+	return &CourseRepository{
+		Provider: provider,
+	}
+}
+
+func (r *CourseRepository) Create(course models.Course) error {
+	result := r.Provider.DB().Create(&course)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *CourseRepository) Update(course models.Course) error {
+	result := r.Provider.DB().Model(course).Updates(course)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *CourseRepository) Find(course models.Course) ([]models.Course, error) {
+	return nil, nil
+}
+
+func (r *CourseRepository) GetAll() ([]models.Course, error) {
+	var courses []models.Course
+	result := r.Provider.DB().Find(&courses)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return courses, nil
+}
+
+func (r *CourseRepository) Get(id, version uint) (models.Course, error) {
+	var course models.Course
+	result := r.Provider.DB().Where("id = ? AND version = ?", id, version).First(&course)
+	if result.Error != nil {
+		return models.Course{}, result.Error
+	}
+	return course, nil
+}
+
+func (r *CourseRepository) Delete(id, version uint) error {
+	result := r.Provider.DB().Where("id = ? AND version = ?", id, version).Delete(&models.Course{})
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
