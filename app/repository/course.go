aed07e808348b5550ed4599e823c581472cefad9
jakob.ferber <jakob.ferber@hftm.ch>
2024-02-09 15:31:32 +0100

    fix: CRUD of course

--- app/repository/course.go
+++ app/repository/course.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"github.com/SmashGrade/backend/app/db"
 	"github.com/SmashGrade/backend/app/models"
-	"gorm.io/gorm/clause"
 )
 
 type CourseRepository struct {
@@ -17,41 +16,7 @@ func NewCourseRepository(provider db.Provider) *CourseRepository {
 	}
 }
 
-func (r *CourseRepository) Create(course models.Course) error {
-	result := r.Provider.DB().Create(&course)
-	if result.Error != nil {
-		return result.Error
-	}
-	return nil
-}
-
-func (r *CourseRepository) Update(course models.Course) error {
-	result := r.Provider.DB().Model(&course).Updates(&course)
-	if result.Error != nil {
-		return result.Error
-	}
-	return nil
-}
-
-func (r *CourseRepository) Find(course models.Course) ([]models.Course, error) {
-	var courses []models.Course
-	result := r.Provider.DB().Preload(clause.Associations).Where(&course).Find(&courses)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return courses, nil
-}
-
-func (r *CourseRepository) GetAll() ([]models.Course, error) {
-	var courses []models.Course
-	result := r.Provider.DB().Preload(clause.Associations).Find(&courses)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return courses, nil
-}
-
-func (r *CourseRepository) Delete(id, version uint) error {
+func (r *CourseRepository) DeleteVersioned(id, version uint) error {
 	result := r.Provider.DB().Where("id = ? AND version = ?", id, version).Delete(&models.Course{})
 	if result.Error != nil {
 		return result.Error
