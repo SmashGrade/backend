f7c03b7037c4b1a68dfab4cf90b4ce7f6a94d743
jakob.ferber <jakob.ferber@hftm.ch>
2024-02-09 16:17:29 +0100

    feat: added conversion repository

---
+++ app/repository/conversion.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/SmashGrade/backend/app/db"
+	"github.com/SmashGrade/backend/app/models"
+	"gorm.io/gorm/clause"
+)
+
+type ConversionRepository struct {
+	Provider db.Provider
+	*BaseRepository
+}
+
+func NewConversionRepository(provider db.Provider) *ConversionRepository {
+	return &ConversionRepository{
+		Provider: provider,
+	}
+}
+
+func (r *ConversionRepository) GetTimed(id uint, startDate time.Time) (entity models.Conversion, err error) {
+	result := r.Provider.DB().Preload(clause.Associations).
+		Where("id = ? AND ee_selected_course_class_startyear = ?", id, startDate).
+		First(&entity)
+	err = result.Error
+	return
+}
+
+func (r *ConversionRepository) DeleteTimed(id uint, startDate time.Time) error {
+	result := r.Provider.DB().Preload(clause.Associations).
+		Where("id = ? AND ee_selected_course_class_startyear = ?", id, startDate).
+		Delete(&models.Conversion{})
+	return result.Error
