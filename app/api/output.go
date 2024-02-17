4a7584104fd57bcc43e0f02030fd055a1274356d
stafel <rafael.stauffer@hftm.ch>
2024-02-17 17:11:43 +0100

    feat: output endpoints

--- app/api/output.go
+++ app/api/output.go
@@ -13,14 +13,20 @@ import (
 type OutputController struct {
 	*BaseController
 	//fieldDao dao.fieldDao
-	stateDao *dao.StateDao
+	stateDao           *dao.StateDao
+	curriculumytypeDao *dao.CurriculumTypeDao
+	gradetypeDao       *dao.GradeTypeDao
+	evaluationtypeDao  *dao.EvaluationTypeDao
 }
 
 // Constructor for output controller
 func NewOutputController(provider db.Provider) *OutputController {
 	return &OutputController{
-		BaseController: NewBaseController(provider),
-		stateDao:       dao.NewStateDao(repository.NewStateRepository(provider)),
+		BaseController:     NewBaseController(provider),
+		stateDao:           dao.NewStateDao(repository.NewStateRepository(provider)),
+		curriculumytypeDao: dao.NewCurriculumTypeDao(repository.NewCurriculumtypeRepository(provider)),
+		gradetypeDao:       dao.NewGradeTypeDao(repository.NewGradetypeRepository(provider)),
+		evaluationtypeDao:  dao.NewEvaluationTypeDao(repository.NewEvaluationtypeRepository(provider)),
 	}
 }
 
@@ -41,7 +47,61 @@ func (c *OutputController) States(ctx echo.Context) error {
 	return c.Yeet(ctx, res)
 }
 
+// @Summary Get all curriculum types
+// @Description Get all curriculum types
+// @Tags curriculumtypes
+// @Produce json
+// @Success 200 {array} models.Curriculumtype
+// @Failure 401 {object} error.ApiError
+// @Failure 403 {object} error.ApiError
+// @Failure 500 {object} error.ApiError
+// @Router /curriculumtypes [get]
+func (c *OutputController) Curriculumtypes(ctx echo.Context) error {
+	res, err := c.curriculumytypeDao.GetAll()
+	if err != nil {
+		return err
+	}
+	return c.Yeet(ctx, res)
+}
+
+// @Summary Get all grade types
+// @Description Get all grade types
+// @Tags gradetypes
+// @Produce json
+// @Success 200 {array} models.Gradetype
+// @Failure 401 {object} error.ApiError
+// @Failure 403 {object} error.ApiError
+// @Failure 500 {object} error.ApiError
+// @Router /gradetypes [get]
+func (c *OutputController) Gradetypes(ctx echo.Context) error {
+	res, err := c.gradetypeDao.GetAll()
+	if err != nil {
+		return err
+	}
+	return c.Yeet(ctx, res)
+}
+
+// @Summary Get all evaluation types
+// @Description Get all evaluation types
+// @Tags evaluationtypes
+// @Produce json
+// @Success 200 {array} models.Evaluationtype
+// @Failure 401 {object} error.ApiError
+// @Failure 403 {object} error.ApiError
+// @Failure 500 {object} error.ApiError
+// @Router /evaluationtypes [get]
+func (c *OutputController) Evaluationtypes(ctx echo.Context) error {
+	res, err := c.evaluationtypeDao.GetAll()
+	if err != nil {
+		return err
+	}
+	return c.Yeet(ctx, res)
+}
+
 // register all output endpoints to router
 func RegisterV1Output(g *echo.Group, o *OutputController) {
 	g.GET("/states", o.States)
+	g.GET("/curriculumtypes", o.Curriculumtypes)
+	g.GET("/gradetypes", o.Gradetypes)
+	g.GET("/evaluationtypes", o.Evaluationtypes)
 }
