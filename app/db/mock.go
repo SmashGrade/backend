1fb408f0756dc8d00197b49d3570ee6d05e8029b
jakob.ferber <jakob.ferber@hftm.ch>
2024-02-17 21:30:37 +0100

    test added unit test for course

--- app/db/mock.go
+++ app/db/mock.go
@@ -10,6 +10,7 @@ import (
 func prefillMockDB(p Provider) {
 	prefillFields(p)
 	prefillConversions(p)
+	prefillCourses(p)
 }
 
 // Field that will be added to the mock DB
@@ -62,10 +63,50 @@ func Conversion_2() models.Conversion {
 	return conversion
 }
 
-// add all the fields to the fields table of the mockDB
+// add all the conversions to the conversions table of the mockDB
 func prefillConversions(p Provider) {
 	conversion_1 := Conversion_1()
 	conversion_2 := Conversion_2()
 	p.DB().Table("conversions").Create(&conversion_1)
 	p.DB().Table("conversions").Create(&conversion_2)
+}
+
+// Course that will be added to the mock DB
+func Course_1() models.Course {
+	var course models.Course
+	course.Description = "Course 1"
+	course.Number = "NR01"
+	course.Version = 1
+	course.ID = 1
+	return course
+}
+
+// Course that will be added to the mock DB
+func Course_2_1() models.Course {
+	var course models.Course
+	course.Description = "Course 2"
+	course.Number = "NR02"
+	course.Version = 1
+	course.ID = 2
+	return course
+}
+
+// Course that will be added to the mock DB
+func Course_2_2() models.Course {
+	var course models.Course
+	course.Description = "Course 2"
+	course.Number = "NR02"
+	course.Version = 2
+	course.ID = 2
+	return course
+}
+
+// add all the courses to the courses table of the mockDB
+func prefillCourses(p Provider) {
+	course_1 := Course_1()
+	course_2_1 := Course_2_1()
+	course_2_2 := Course_2_2()
+	p.DB().Table("courses").Create(&course_1)
+	p.DB().Table("courses").Create(&course_2_1)
+	p.DB().Table("courses").Create(&course_2_2)
 }
