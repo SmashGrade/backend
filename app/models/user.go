d7dee283ef8ba5b63cb4a1fa770777b2f15dec43
jakob.ferber <jakob.ferber@hftm.ch>
2024-02-18 11:51:42 +0100

    refactor user email is know unique

--- app/models/user.go
+++ app/models/user.go
@@ -8,7 +8,7 @@ type User struct {
 	Basemodel
 	ClassStartyear  time.Time        `json:"classStartYear"`
 	Name            string           `json:"name"`
-	Email           string           `json:"email"`
+	Email           string           `gorm:"unique" json:"email"`
 	Fields          []*Field         `gorm:"many2many:fieldmanager;" json:"fields"`
 	Roles           []*Role          `gorm:"many2many:user_has_role;" json:"roles"`
 	TeachesCourses  []*Course        `gorm:"many2many:course_teacher;" json:"teachesCourses"`
