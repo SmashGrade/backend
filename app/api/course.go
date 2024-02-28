1689c7957f0966fe68d295832fff6e7e6de7e2ce
Stafel <rafael.stauffer@hftm.ch>
2024-02-28 20:03:36 +0100

    feat: add create course version, delete course

--- app/api/course.go
+++ app/api/course.go
@@ -110,6 +110,47 @@ func (c *CourseController) Create(ctx echo.Context) error {
 	return c.Yeet(ctx, returnCourse)
 }
 
+// @Summary		Create a new version of a course
+// @Description	Create a new version of a course
+// @Tags			courses
+// @Produce		json
+// @Accept			json
+//
+// @Param			request	body		requestmodels.RefCourse	true	"request body"
+// @Param			id		path	uint	true	"Course ID"
+//
+// @Success		200		{object}	models.Course
+// @Failure		401		{object}	error.ApiError
+// @Failure		403		{object}	error.ApiError
+// @Failure		500		{object}	error.ApiError
+// @Router			/courses [post]
+// @Security		Bearer
+func (c *CourseController) CreateVersion(ctx echo.Context) error {
+	// Read id parameter from request
+
+	id, err := c.GetPathParamUint(ctx, "id")
+	if err != nil {
+		return e.NewDaoValidationError("id", "uint", c.GetPathParam(ctx, "id"))
+	}
+
+	course := new(requestmodels.RefCourse)
+	// Read the request into Course
+	if err := ctx.Bind(course); err != nil {
+		return e.ErrorInvalidRequest("course")
+	}
+
+	course.ID = id
+	course.Version = 0
+
+	// Let dao create the Course
+	returnCourse, err := c.Dao.Create(*course)
+	if err != nil {
+		return err
+	}
+	// return the result from the Post
+	return c.Yeet(ctx, returnCourse)
+}
+
 // @Summary		Update a course
 // @Description	Update a course
 // @Tags			courses
@@ -128,7 +169,6 @@ func (c *CourseController) Create(ctx echo.Context) error {
 // @Security		Bearer
 func (c *CourseController) Update(ctx echo.Context) error {
 	// Read id parameter from request
-
 	id, err := c.GetPathParamUint(ctx, "id")
 	if err != nil {
 		return e.NewDaoValidationError("id", "uint", c.GetPathParam(ctx, "id"))
@@ -157,10 +197,49 @@ func (c *CourseController) Update(ctx echo.Context) error {
 	return c.Yeet(ctx, course)
 }
 
+// @Summary		Delete a course
+// @Description	Delete a course
+// @Tags			courses
+// @Produce		json
+// @Accept			json
+//
+// @Param			id		path	uint	true	"Course ID"
+// @Param			version	path	uint	true	"Course Version"
+//
+// @Success		200
+// @Failure		401		{object}	error.ApiError
+// @Failure		403		{object}	error.ApiError
+// @Failure		500		{object}	error.ApiError
+// @Router			/courses [delete]
+// @Security		Bearer
+func (c *CourseController) Delete(ctx echo.Context) error {
+	// Read id parameter from request
+
+	id, err := c.GetPathParamUint(ctx, "id")
+	if err != nil {
+		return e.NewDaoValidationError("id", "uint", c.GetPathParam(ctx, "id"))
+	}
+
+	version, err := c.GetPathParamUint(ctx, "version")
+	if err != nil {
+		return e.NewDaoValidationError("version", "uint", c.GetPathParam(ctx, "version"))
+	}
+
+	// Let dao create the Course
+	err = c.Dao.Delete(id, version)
+	if err != nil {
+		return err
+	}
+	// return the result from the Post
+	return c.Yeet(ctx, nil)
+}
+
 // register all output endpoints to router
 func RegisterV1Courses(g *echo.Group, c *CourseController) {
 	g.GET("/courses", c.Courses)
 	g.GET("/courses/:id/:version", c.Course)
 	g.POST("/courses", c.Create)
+	g.POST("/courses/:id", c.CreateVersion)
 	g.PUT("/courses/:id/:version", c.Update)
+	g.DELETE("/courses/:id/:version", c.Delete)
 }
